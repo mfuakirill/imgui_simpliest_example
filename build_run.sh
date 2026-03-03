10c1fac263aeb24c09e36f147fd6ea0c05375410
mfuakirill <29417670@s.mfua.ru>
2026-03-03 15:31:24 +0600

    Up

---
+++ build_run.sh
@@ -0,0 +1,6 @@
+root=$(pwd)
+mkdir build; cd build
+cmake .. -G "MinGW Makefiles" -DCMAKE_C_COMPILER=C:/mingw64/bin/gcc.exe -DCMAKE_CXX_COMPILER=C:/mingw64/bin/g++.exe
+mingw32-make.exe
+./imgui_example_claude.exe
+cd $root
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello ImGui", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Your UI here
+        ImGui::Begin("My Window");
+        ImGui::Text("Hello, World!");
+        if (ImGui::Button("Click me"))
+            ImGui::Text("Clicked!");
+        ImGui::End();
+
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
